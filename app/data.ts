d86223b103626f19c78bdb694b9b18aca6825113
Volodymyr Grebennikov <vgrebennikov@hopeukraine.net>
2025-10-31 01:37:25 +0200

    Complete site for church QR codes

--- app/data.ts
+++ app/data.ts
@@ -9,28 +9,28 @@ export const data = [
     { 
         "id": 2, 
         "title": "Наш Youtube канал",
-        "link": "https://t.me/church_savior",
+        "link": "https://www.youtube.com/@drohobych.church",
         "iconBlack": "/icons/iconYoutube.png",
-        "iconWhite": "/icons/iconYoutubeWhite.png"
+        "iconWhite": "/icons/iconsYoutubeWhite.png"
     },
     { 
         "id": 3, 
         "title": "Наша Instagram сторінка церкви",
-        "link": "https://t.me/church_savior",
+        "link": "https://www.instagram.com/drohobych.church",
         "iconBlack": "/icons/iconInstagram.png",
         "iconWhite": "/icons/iconInstagramWhite.png"
     },
     { 
         "id": 4, 
         "title": "Instagram сторінка нашої молоді",
-        "link": "https://t.me/church_savior",
+        "link": "https://www.instagram.com/drohobych.youth/",
         "iconBlack": "/icons/iconInstagram.png",
         "iconWhite": "/icons/iconInstagramWhite.png"
     },
     { 
         "id": 5, 
         "title": "Тelegram канал молоді",
-        "link": "https://t.me/church_savior",
+        "link": "https://t.me/drohobych_youth",
         "iconBlack": "/icons/iconTelegram.png",
         "iconWhite": "/icons/iconTelegramWhite.png"
     }
