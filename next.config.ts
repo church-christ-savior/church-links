5b5341334ebc0d97ab71bb2d0ef4dd7fc7c585c0
Hrebennikov <valerii.hrebennikov@hebron-academy.com>
2025-10-31 11:37:56 +0200

    add GitHub Pages deploy action

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', 
-  images: {
-    unoptimized: true, 
-  },
+  output: 'export',
+  images: { unoptimized: true },
   basePath: '/church_links', 
+  assetPrefix: '/church_links/', 
 };
 
 export default nextConfig;
